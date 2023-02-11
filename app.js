442cf03aaec48f92aa2620f61da012e0c152816a
anhnc <spook-email-05@icloud.com>
2023-02-12 02:03:22 +0700

    fix dirname

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Serve files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
